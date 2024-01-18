1faaa1575bccd0f193aa0d2c639d9c97a957599a
jackent601 <jackent601@gmail.com>
2024-01-18 17:32:18 +0000

    scenarios

--- wifiphisher_broker/views.py
+++ wifiphisher_broker/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 # from django.utils import timezone
 from portal_auth.views import get_session_from_request
 
+from wifiphisher_broker.wifiphisher_config import PHISHING_SCENARIOS
+
 # import glob, os, datetime, time, subprocess
 
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
@@ -19,13 +21,25 @@ def wifiphisher_captive_portal_home(request):
         message=messages.error(request, "No active session for user, log out and in again to create a session")
         return redirect('home')
     
-    # Prepare form to config captive portal
+    # Check current status
+    # status, wphisher_session = get_current_wphisher_session()
+    # wphisher_config["captive_portal_status"]=status 
+    
+    # Prepare Config
     wphisher_config={}
+    # Interfaces
     interfaces = get_interfaces()
     if len(interfaces) == 0:
         message=messages.error(request, "No available interfaces for captive portal session, check platform")
         return redirect('home')   
-    wphisher_config["interfaces"]=interfaces   
+    wphisher_config["interfaces"]=interfaces  
+    
+    # Scenarios
+    scenarios = get_scenarios()
+    if len(scenarios) == 0:
+        message=messages.error(request, "No available scenarios for captive portal session, check platform")
+        return redirect('home')   
+    wphisher_config["scenarios"]=scenarios  
       
     return render(request, 'wifiphisher_broker/captive_portal_home.html', wphisher_config)
 
@@ -40,3 +54,13 @@ def get_interfaces()->[str]:
     """
     # TODO - implement properaly
     return ["wlan0", "wlan1"]
+
+def get_scenarios()->[str]:
+    """
+    Lists scenarios available for wifiphisher captive portal. Declared in wifiphisher config
+    TODO - Perhaps could be acertained from the tool itself?
+    """
+    # TODO - implement properaly
+    return PHISHING_SCENARIOS.keys()
+
+# def get_current_wphisher_session():
