df904b9bbdcc28a06b34932aa88f5350008adf4e
jackent601 <jackent601@gmail.com>
2024-05-05 22:33:04 +0100

    setup

--- lelantos/settings.py
+++ lelantos/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = 'django-insecure-#f_)rvdq4a*u64hkz=j#ydg&i&kwj^6pgrco_-sl=o17%)m8!(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = ["192.168.0.76", "127.0.0.1"]
-
+if DEBUG:
+    ALLOWED_HOSTS=["*"]
 
 # Application definition
 
